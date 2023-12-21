d547c6c459e8e94a0029b17573973295cc46fe50
Nevin Toms <nevintoms333@gmail.com>
2023-12-21 14:45:17 -0500

    Add projects component

---
+++ components/projects/Projects.tsx
@@ -0,0 +1,20 @@
+import Card from "@/components/card/Card"
+
+export default function Projects() {
+    return (
+        <div className="">
+            <p className="text-center font-bold py-6">Projects</p>
+            <div className="grid grid-cols-3 gap-4">
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+                <Card title="Card title" description="Desciption" />
+            </div>
+        </div>
+    )
+}
