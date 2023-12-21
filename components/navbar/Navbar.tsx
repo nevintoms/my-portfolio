c1c7567333f0f33e5fcf728dbd2fab6b642d9b51
Nevin Toms <nevintoms333@gmail.com>
2023-12-21 13:39:25 -0500

    Add navbar

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <div className="flex justify-center font-light items-center drop-shadow-xl p-4 bg-black w-full text-gray-300">
+            <a href="" className="px-2 hover:text-white transition duration-300 ease-in-out">About</a>
+            <a href="" className="px-2 hover:text-white transition duration-300 ease-in-out">Resume</a>
+            <a href="" className="px-2 hover:text-white transition duration-300 ease-in-out">Projects</a>
+            <a href="" className="px-2 hover:text-white transition duration-300 ease-in-out">Contact</a>
+        </div>
+    )
+}
